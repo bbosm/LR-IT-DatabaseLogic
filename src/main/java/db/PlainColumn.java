b14e1f1b8d54e8cc35e3bc938a1b21510186be40
Roman <romashka1996@gmail.com>
2017-10-21 18:27:07 +0300

    Constructor is serialized

--- src/main/java/db/PlainColumn.java
+++ src/main/java/db/PlainColumn.java
@@ -2,16 +2,34 @@ package db;
 
 import dbtype.Attribute;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
 
 public class PlainColumn extends Column {
 
-    private Constructor stringConstructor;
+    private transient Constructor stringConstructor;
+
+    private void writeObject(ObjectOutputStream stream)
+            throws IOException {
+        stream.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream stream)
+            throws IOException, ClassNotFoundException, NoSuchMethodException {
+        stream.defaultReadObject();
+        start();
+    }
 
     public PlainColumn(String s) throws
             NoSuchMethodException,
             ClassNotFoundException {
         super(s);
+        start();
+    }
+
+    private void start() throws ClassNotFoundException, NoSuchMethodException {
         this.stringConstructor = Class.forName(
                 Attribute.getFullTypeName(getAttributeShortTypeName())).getConstructor(String.class);
     }
